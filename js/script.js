8692f144a672728e81dc6e4ccaa14a77ef4577ae
KamillaR <124995176+OGsnapzilla@users.noreply.github.com>
2024-05-22 12:57:15 +0200

    nav started, images added

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// NAVIGATION FUNCTIONS
+function hideSidebar() {
+    //document = HTML dokumentet
+    //querySelector = søger efter et element
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none';
+}
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
